371d71b4140f8b87cd836af45da194b8ff0309c1
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-10-18 18:03:23 +0200

    fuctions random and pair or not

--- 02- pariDispari/script.js
+++ 02- pariDispari/script.js
@@ -1,9 +1,14 @@
 //* Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
 // Sommiamo i due numeri
+
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
 // Dichiariamo chi ha vinto.
+
 // Consigli del giorno
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
@@ -13,4 +18,15 @@
 // 3. Devo restituire un valore?
 // 4. Se sì, di che tipo?
 // Buon lavoro e buon divertimento!
+
+function randomNumberPc(){
+
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+function isPairOrNot(number){
+    return number % 2 === 0 ? 'il numero e pari' : 'il numero e dispari';
+}
+
+
 
